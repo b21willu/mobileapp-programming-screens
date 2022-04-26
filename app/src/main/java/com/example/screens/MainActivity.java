427fca58b22bf3a12f020648335e6c962f1627a9
b21willu <Wilmer990302>
2022-04-26 13:08:52 +0200

    Added code that starts the other activity when button is pressed.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button b = findViewById(R.id.signin_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
