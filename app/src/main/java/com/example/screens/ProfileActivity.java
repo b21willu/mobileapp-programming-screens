70a1de5f33502f183e8af76ec244a9d14c4aca29
b21willu <Wilmer990302>
2022-04-26 15:05:01 +0200

    Added TextView widget to ProfileActivity. Added code to show data from intent in TextView widget.

--- app/src/main/java/com/example/screens/ProfileActivity.java
+++ app/src/main/java/com/example/screens/ProfileActivity.java
@@ -3,19 +3,28 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView nameText, ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile2);
 
+
+        nameText = findViewById(R.id.UserName);
+        ageText = findViewById(R.id.Age);
+
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             String name = extras.getString("name");
             int number = extras.getInt("number");
             // Do something with the name and number
+            nameText.setText("Hi! "+name);
+            ageText.setText("Your age is: "+number);
         }
     }
 }
